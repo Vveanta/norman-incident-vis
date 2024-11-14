84b86c6df7c55b3f98e37a4feebb9e0ce825af46
Vveanta <104909463+Vveanta@users.noreply.github.com>
2024-11-14 00:05:43 -0500

    updated run.py to take environment variable

--- run.py
+++ run.py
@@ -11,6 +11,10 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(debug=True, host="0.0.0.0", port=5200)
-    app.run(debug=True, port=5200)  
+    port = int(os.environ.get('PORT', 8000))
+    if os.environ.get('FLASK_ENV') == 'production':
+        serve(app, host='0.0.0.0', port=port)
+    else:
+        app.run(debug=True, host='0.0.0.0', port=port)
     # serve(app, host="0.0.0.0", port=5200)
     # app.run(debug=True)  
